ed77670e10cc858a3c24259c292d2bb6ed7121a9
Lars Kleen <extern.lars.kleen@audi.de>
2024-11-13 22:45:48 +0100

    adapted example to compile with openhab 4.2.2

---
+++ src/main/java/org/openhab/automation/jrule/rules/user/FireplaceToggleRule.java
@@ -0,0 +1,26 @@
+package org.openhab.automation.jrule.rules.user;
+
+import org.openhab.automation.jrule.generated.items.JRuleItems;
+import org.openhab.automation.jrule.rules.JRule;
+import org.openhab.automation.jrule.rules.JRuleName;
+import org.openhab.automation.jrule.rules.JRuleWhenItemChange;
+import org.openhab.automation.jrule.rules.value.JRuleOnOffValue;
+
+import static org.openhab.automation.jrule.generated.items.JRuleItemNames.fireplace_switch_Action;
+
+public class FireplaceToggleRule extends JRule {
+
+    @JRuleName("fireplace_desk_light_toggle")
+    @JRuleWhenItemChange(item = fireplace_switch_Action)
+    public boolean toggleSwitch() {
+        logInfo("hooray it has been executed!!!");
+        JRuleOnOffValue value = JRuleItems.fireplace_light_desk_PowerSwitch.getStateAsOnOff();
+        if (value == JRuleOnOffValue.ON) {
+            JRuleItems.fireplace_light_desk_PowerSwitch.sendCommand(JRuleOnOffValue.OFF);
+        } else {
+            JRuleItems.fireplace_light_desk_PowerSwitch.sendCommand(JRuleOnOffValue.ON);
+        }
+        return true;
+    }
+
+}
--- src/test/java/org/openhab/automation/jrule/rules/user/FireplaceToggleRuleTest.java
+++ src/test/java/org/openhab/automation/jrule/rules/user/FireplaceToggleRuleTest.java
@@ -1,28 +1,28 @@
 package org.openhab.automation.jrule.rules.user;
 
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import org.apache.commons.collections4.map.HashedMap;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.openhab.automation.jrule.internal.JRuleConfig;
 import org.openhab.automation.jrule.internal.engine.JRuleEngine;
+import org.openhab.automation.jrule.internal.module.JRuleRuleProvider;
 import org.openhab.automation.jrule.internal.test.JRuleMockedEventBus;
-import org.openhab.automation.jrule.rules.JRuleEvent;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 
 @ExtendWith(MockitoExtension.class)
-public class TestMyRules {
+public class FireplaceToggleRuleTest {
+
+    private FireplaceToggleRule fireplaceToggleRule = new FireplaceToggleRule();
 
     @Mock
-    private MyFirstRules myFirstRules;
+    private JRuleRuleProvider ruleProvider;
 
     public void setUp() throws Exception {
         Logger rootLogger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
@@ -34,9 +34,10 @@ public class TestMyRules {
         JRuleEngine engine = JRuleEngine.get();
         JRuleConfig config = new JRuleConfig(new HashedMap<>());
         engine.setConfig(config);
-        engine.add(myFirstRules);
-        JRuleMockedEventBus eventBus = new JRuleMockedEventBus("eventlog.txt");
+        engine.setRuleProvider(ruleProvider);
+        engine.add(fireplaceToggleRule, true);
+        JRuleMockedEventBus eventBus = new JRuleMockedEventBus();
         eventBus.start();
-        verify(myFirstRules, times(2)).myFirstRule(Mockito.any(JRuleEvent.class));
+        verify(fireplaceToggleRule.toggleSwitch()).equals(true);
     }
 }
