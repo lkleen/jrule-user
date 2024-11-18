0d933df50cc0b1d26fb6c7072a062bbe2fb4bfe4
Lars Kleen <extern.lars.kleen@audi.de>
2024-11-18 21:46:24 +0100

    implement working toggle rule

--- src/main/java/org/openhab/automation/jrule/rules/user/FireplaceToggleRule.java
+++ src/main/java/org/openhab/automation/jrule/rules/user/FireplaceToggleRule.java
@@ -1,9 +1,7 @@
 package org.openhab.automation.jrule.rules.user;
 
 import org.openhab.automation.jrule.generated.items.JRuleItems;
-import org.openhab.automation.jrule.rules.JRule;
-import org.openhab.automation.jrule.rules.JRuleName;
-import org.openhab.automation.jrule.rules.JRuleWhenItemChange;
+import org.openhab.automation.jrule.rules.*;
 import org.openhab.automation.jrule.rules.value.JRuleOnOffValue;
 
 import static org.openhab.automation.jrule.generated.items.JRuleItemNames.fireplace_switch_Action;
@@ -11,7 +9,7 @@ import static org.openhab.automation.jrule.generated.items.JRuleItemNames.firepl
 public class FireplaceToggleRule extends JRule {
 
     @JRuleName("fireplace_desk_light_toggle")
-    @JRuleWhenItemChange(item = fireplace_switch_Action)
+    @JRuleWhenItemReceivedUpdate(item = fireplace_switch_Action)
     public boolean toggleSwitch() {
         logInfo("hooray it has been executed!!!");
         JRuleOnOffValue value = JRuleItems.fireplace_light_desk_PowerSwitch.getStateAsOnOff();
